4e1b81beeae717e15b87fa7880e0e31dd31f7f62
Greg Rotter <grotter@gmail.com>
2021-01-04 10:07:08 -0800

    removing some cruft

--- .platform/hooks/postdeploy/99-delayed.sh
+++ .platform/hooks/postdeploy/99-delayed.sh
@@ -3,16 +3,6 @@
 CALACADEMY_DIR="displays.calacademy.org"
 CALACADEMY_ENVIRONMENT=$(/opt/elasticbeanstalk/bin/get-config container | /usr/bin/jq -r .environment_name)
 
-if [ "$CALACADEMY_ENVIRONMENT" == "calacademy-displays-stg" ];
-then
-  CALACADEMY_DIR="displays-stg.calacademy.org"
-fi
-
-if [ "$CALACADEMY_ENVIRONMENT" == "calacademy-displays-dev" ];
-then
-  CALACADEMY_DIR="displays-dev.calacademy.org"
-fi
-
 # customize the Message of the Day
 if [ ! -f /etc/motd.tail ];
 then
